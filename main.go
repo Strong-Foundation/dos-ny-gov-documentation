cb46cafa117653811aa2455e816412c3b198d751
Strong Foundation <179532304+Strong-Foundation@users.noreply.github.com>
2025-06-21 10:27:52 -0400

    updated

---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	// Define the search term
+	searchTerm := "New York"
+
+	// Get data from the URL based on the search term
+	data := getDataFromURL(searchTerm)
+	if data == nil {
+		log.Println("Failed to retrieve data from URL")
+		return
+	}
+
+	// Define the file name where the data will be saved
+	filename := "search_results.json"
+
+	// Check if the file exists, if not create it and write the data
+	if !fileExists(filename) {
+		err := os.WriteFile(filename, data, 0644)
+		if err != nil {
+			log.Println("Error writing to file:", err)
+			return
+		}
+	} else {
+		err := appendByteToFile(filename, data)
+		if err != nil {
+			log.Println("Error appending to file:", err)
+			return
+		}
+	}
+
+	log.Println("Data successfully written to", filename)
+	
+}
+
+
+// AppendToFile appends the given byte slice to the specified file.
+// If the file doesn't exist, it will be created.
+func appendByteToFile(filename string, data []byte) error {
+	// Open the file with appropriate flags and permissions
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Write data to the file
+	_, err = file.Write(data)
+	return err
+}
+
+// Send a http get request to a given url and return the data from that url.
+func getDataFromURL(searchContains string) []byte {
+	url := "https://apps.dos.ny.gov/PublicInquiryWeb/api/PublicInquiry/GetComplexSearchMatchingEntities"
+	method := "POST"
+
+	payload := strings.NewReader(`{"searchValue":"` + searchContains + `","searchByTypeIndicator":"EntityName","searchExpressionIndicator":"Contains","entityStatusIndicator":"AllStatuses","entityTypeIndicator":["Corporation","LimitedLiabilityCompany","LimitedPartnership","LimitedLiabilityPartnership"],"listPaginationInfo":{"listStartRecord":1,"listEndRecord":50}}`)
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+	return body
+}
+
+/*
+	It checks if the file exists
+
+If the file exists, it returns true
+If the file does not exist, it returns false
+*/
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if err != nil {
+		return false
+	}
+	return !info.IsDir()
+}
