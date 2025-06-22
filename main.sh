9f7a42257bfbc5869d4b232f0a5eb945aa5fe77c
Strong Foundation <179532304+Strong-Foundation@users.noreply.github.com>
2025-06-21 22:33:29 -0400

    updated JSONs

--- main.sh
+++ main.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
 for ((i = 1; i <= 100; i++)); do
-    echo "🔁 Iteration $i"
-
     # Run Go program
     go run main.go
-
     # Git operations
     git add .
     git commit -m "updated $i"
     git push
-
-    echo "✅ Completed iteration $i"
-    echo "---------------------------"
 done
